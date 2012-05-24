e9a6c77a7b130ea2e82ad1d1b622209b12dbfa4f
Jason Staten <jstaten07@gmail.com>
2012-05-24 09:32:46 -0600

    initial commit

---
+++ mongomerge.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'uri'
+require 'tmpdir'
+
+def split_uri(uri)
+  parts = URI.parse(uri)
+  raise "scheme must be mongodb, found #{parts.scheme}" unless parts.scheme == 'mongodb'
+  info = {
+    host:     parts.host,
+    port:     parts.port,
+    database: parts.path.gsub(/^\//, ''),
+    username: parts.user,
+    password: parts.password
+  }
+end
+
+def merge source_uri, target_uri
+  target = split_uri target_uri
+  source = split_uri source_uri
+
+  Dir::Tmpname.create('dump') do |path|
+    system "mongodump -h #{source[:host]} --port #{source[:port]} -u #{source[:username]} -p #{source[:password]} --db #{source[:database]} --out #{path}"
+    system "mongorestore -h #{target[:host]} #{"--port #{target[:port]}" if target[:port]} #{"-u #{target[:username]}" if target[:username]} #{"-p #{target[:password]}" if target[:password]} --db #{target[:database]} #{path}/#{source[:database]}"
+  end
+end
+
+if ARGV.length != 2
+  puts 'Usage: mongomerge source_uri target_uri'
+  exit 1
+end
+
+source_uri, target_uri = ARGV
+puts "Merging\n#{source_uri} into\n#{target_uri}\nCorrect[yes/no]?"
+response = $stdin.gets.chomp
+exit 1 unless response.downcase == 'yes'
+
+merge(ARGV[0], ARGV[1])
